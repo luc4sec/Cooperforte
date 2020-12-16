a3d730ad559093bf1d010cf6c901d248bbc0113f
Lucas Araújo <55511610+llucasramao@users.noreply.github.com>
2020-12-16 16:07:26 -0300

    0.0.2
    Adicionado sssd

--- setup.sh
+++ setup.sh
@@ -11,11 +11,35 @@ if [ "$1" == "" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
         echo ""
 elif [ "$1" == "install" ]; then
         if [ `whoami` == 'root' ]; then
+		echo "Atualizando pacotes"
+		sudo apt update; sudo apt list --upgradable; sudo apt upgrade -y; sudo apt autoremove -y
                 echo "Instalando aplicativos..."
-		apt -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit & > log.log
+		apt -y install realmd sssd sssd-tools libnss-sss libpam-sss adcli samba-common-bin oddjob oddjob-mkhomedir packagekit
                 echo "Digite o usuário de rede: "
                 read user
                 realm join --user $user cooperforte.coop
+		mv /etc/sssd/sssd.conf /etc/sssd/sssd.conf.old
+		echo "
+[sssd]
+domains = cooperforte.coop
+config_file_version = 2
+services = nss, pam
+
+[domain/cooperforte.coop]
+ad_domain = cooperforte.coop
+krb5_realm = COOPERFORTE.COOP
+realmd_tags = manages-system joined-with-adcli
+cache_credentials = True
+id_provider = ad
+krb5_store_password_if_offline = True
+default_shell = /bin/bash
+ldap_id_mapping = true
+use_fully_qualified_names = false
+fallback_homedir = /home/%u
+access_provider = ad
+ad_gpo_access_control = permissive" > /etc/sssd/sssd.conf
+
+		
         else
                 echo "É necessário logar com root (sudo ./setup install)"
         fi
